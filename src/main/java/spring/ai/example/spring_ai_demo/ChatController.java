8d86200780b7dcf652568240b7c10eb7f3fd4110
zhengkanyan <zhengkanyan@gmail.com>
2025-08-22 02:37:19 +0800

    Deepseek Spring AI Demo

---
+++ src/main/java/spring/ai/example/spring_ai_demo/ChatController.java
@@ -0,0 +1,34 @@
+package spring.ai.example.spring_ai_demo;
+
+import java.util.Map;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+
+    private final DeepSeekChatModel chatModel;
+
+    public ChatController(DeepSeekChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+	public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        var prompt = new Prompt(new UserMessage(message));
+        return chatModel.stream(prompt);
+    }
+}
